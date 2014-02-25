65e0de5f7f3be142998a0d02fc2059eef5c49ea8
Jose Fernando Tolentino <JoseFernandoTolentino@gmail.com>
2014-02-25 12:14:04 -0300

    Versao sem jquery

---
+++ static/neto.js
@@ -0,0 +1,34 @@
+﻿
+function atualiza_nome()
+{
+  var nome=pega_nome();
+  if (nome)
+  {   
+     seta('Olá: '+nome); 
+	 document.getElementById("nome").setAttribute("class","azul");
+  }	 
+  else
+  {
+    seta('');
+	 document.getElementById("nome").setAttribute("class","vermelho");
+  }	 
+}
+
+function pega_nome()
+{
+  var elemento_nome = document.getElementById("nome");
+  var nome_digitado = elemento_nome.value;
+  return nome_digitado;
+}
+
+function seta(texto_de_ola)
+{
+  var elemento_ola = document.getElementById("ola1");
+  elemento_ola.innerHTML = texto_de_ola;
+  var elemento_ola = document.getElementById("ola2");
+  elemento_ola.innerHTML = texto_de_ola;
+  var elemento_ola = document.getElementById("ola3");
+  elemento_ola.innerHTML = texto_de_ola;
+  var elemento_ola = document.getElementById("ola4");
+  elemento_ola.innerHTML = texto_de_ola;
+}
