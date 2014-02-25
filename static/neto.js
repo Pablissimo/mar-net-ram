c4bf78c5eccef3fab0daf8e5be42299ff6bd94e6
Jose Fernando Tolentino <JoseFernandoTolentino@gmail.com>
2014-02-25 12:15:13 -0300

    versao com jquery e com localStorage

--- static/neto.js
+++ static/neto.js
@@ -1,34 +1,39 @@
 ﻿
+$( document ).ready(function() {
+  le();
+});
+
+function le()
+{
+   var nome = localStorage.getItem('nome'); 
+   if (nome)
+     $("#nome").val(nome);
+}
+
+function gravar()
+{
+   var nome=$("#nome").val();
+   localStorage.setItem('nome', nome); 
+}
+
 function atualiza_nome()
 {
-  var nome=pega_nome();
+  var nome=$("#nome").val();
   if (nome)
   {   
      seta('Olá: '+nome); 
-	 document.getElementById("nome").setAttribute("class","azul");
+	 $("#nome").removeClass("vermelho");
+	 $("#nome").addClass("azul");
   }	 
   else
   {
-    seta('');
-	 document.getElementById("nome").setAttribute("class","vermelho");
+     seta('');
+	 $("#nome").removeClass("azul");
+	 $("#nome").addClass("vermelho");
   }	 
-}
-
-function pega_nome()
-{
-  var elemento_nome = document.getElementById("nome");
-  var nome_digitado = elemento_nome.value;
-  return nome_digitado;
 }
 
 function seta(texto_de_ola)
 {
-  var elemento_ola = document.getElementById("ola1");
-  elemento_ola.innerHTML = texto_de_ola;
-  var elemento_ola = document.getElementById("ola2");
-  elemento_ola.innerHTML = texto_de_ola;
-  var elemento_ola = document.getElementById("ola3");
-  elemento_ola.innerHTML = texto_de_ola;
-  var elemento_ola = document.getElementById("ola4");
-  elemento_ola.innerHTML = texto_de_ola;
+  $("#ola1,#ola2,#ola3,#ola4").html(texto_de_ola);
 }
