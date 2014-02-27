2f12e99b030a67038cda506b3a0ce3ba67fc7c86
joseaugustoneto <joseneto.design@gmail.com>
2014-02-27 00:44:54 -0300

    Com os arquivos jquery.js scripts.js e style.css

---
+++ static/neto/scripts.js
@@ -0,0 +1,36 @@
+﻿
+$( document ).ready(function() {
+  le();
+});
+
+function le()
+{
+   var nome = localStorage.getItem('nome'); 
+   if (nome)
+     $("#nome").val(nome);
+}
+
+
+function grava_nome()
+{
+   var nome=$("#nome").val();
+   localStorage.setItem('nome', nome); 
+}
+
+function atualizar_o_nome()
+{
+  var nome=$("#nome").val();
+  if (nome)
+  {   
+     seta('Oi, '+nome); 
+  }	 
+  else
+  {
+     seta('');
+  }	 
+}
+
+function seta(texto_de_ola)
+{
+  $("#zero_graus, #noventa_graus, #cento_oitente_graus, #duzentos_setenta_graus").html(texto_de_ola);
+}
