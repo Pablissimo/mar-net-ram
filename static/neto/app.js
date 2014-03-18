dd0449f9d9ab3bbe6befcc92f63bb2f7db389ff7
joseaugustoneto <joseneto.design@gmail.com>
2014-03-18 13:40:51 -0300

    Exemplo de WebSocket... não consegui criar nada alem disso! esse exemplo eu peguei da internet!

--- static/neto/app.js
+++ static/neto/app.js
@@ -1,50 +1,74 @@
-var mvcApp = angular.module('filtersApp', []);
+window.onload = function() {
 
-mvcApp.controller('controller', function($scope) {
+  //Ter referências a elementos da pagina
+  var form = document.getElementById('message-form');
+  var messageField = document.getElementById('message');
+  var messagesList = document.getElementById('messages');
+  var socketStatus = document.getElementById('status');
+  var closeBtn = document.getElementById('close');
+  
 
-  $scope.data = new Date();
 
-  $scope.letras = "abcdefghijklmnopqrstuvwyz";
+  // Cria um WebSocket
+  var socket = new WebSocket('ws://echo.websocket.org');
 
-  $scope.filtro = "";
 
-  $scope.states = [{
-      name: 'Pedro Mariano da Silva',
-      cidade: 'Belo Horizonte',
-      fone: 3420921001
-    }, {
-      name: 'Mariana Barbosa ',
-      cidade: 'São Paulo',
-      fone: 1430923301
-    }, {
-      name: 'Kleber Augusto',
-      cidade: 'Aparecida de Goiânia',
-      fone: 6230002334
-    }, {
-      name: 'Moises Pereira',
-      cidade: 'Rio de Janeiro',
-      fone: 2120923349
-    }, {
-      name: 'João Paulo Barbosa',
-      cidade: 'São Paulo',
-      fone: 1432988833
-    },
-    {
-      name: 'Margarida',
-      cidade: 'Guápo',
-      fone: 6235678823
-    }
-  ];
-
-  $scope.addState = function() {
-    $scope.states.push({
-      name: $scope.nome,
-      cidade: $scope.cidade,
-      fone: 1000
-    });
-    $scope.nome = '';
-    $scope.cidade = '';
-    $scope.fone = '';
+  //Manipulador de erros ocorridos
+  socket.onerror = function(error) {
+    console.log('WebSocket erro: ' + error);
   };
 
-});
+
+  // Mensagem que aparece se for conectado ao servidor.
+  socket.onopen = function(event) {
+    socketStatus.innerHTML = 'Conectado a: ws://echo.websocket.org';
+    socketStatus.className = 'open';
+  };
+
+
+  //Escreve mensagens na tela que foram enviadas pelo servidor.
+  socket.onmessage = function(event) {
+    var message = event.data;
+    messagesList.innerHTML += '<li class="received"><span>Recebido:</span>' +
+                               message + '</li>';
+  };
+
+
+  // Mensagem que aparece quando for desconectado do servidor.
+  socket.onclose = function(event) {
+    socketStatus.innerHTML = 'Desconectado do WebSocket';
+    socketStatus.className = 'closed';
+  };
+
+
+  //Envia a mensagem pelo formulário
+  form.onsubmit = function(e) {
+    e.preventDefault();
+
+    // Recupera a mensagem do TextArea
+    var message = messageField.value;
+
+    //  envia a mensagem através do WebSocket
+    socket.send(message);
+
+    // Adiciona a mensagem na lista
+    messagesList.innerHTML += '<li class="sent"><span>Enviado:</span>' + message +
+                              '</li>';
+
+    // Limpa o textarea
+    messageField.value = '';
+
+    return false;
+  };
+
+
+  // Fecha a conecção Websocket quando é clicado no botão.
+  closeBtn.onclick = function(e) {
+    e.preventDefault();
+
+    // Fecha WebSocket
+    socket.close();
+
+    return false;
+  };
+};
