e879764ab3f2751ef240eb815880826a88afdf3e
joseaugustoneto <joseneto.design@gmail.com>
2014-03-19 09:59:50 -0300

    Exemplo de WebWorker... Duas atividades sendo feitas simultaneamente uma que é a contagem sendo feita pelo WebWorker, e a outra pelo JS sem WebWorker pelo JS.

--- static/neto/app.js
+++ static/neto/app.js
@@ -1,74 +1,42 @@
-window.onload = function() {
+var w;
 
-  //Ter referências a elementos da pagina
-  var form = document.getElementById('message-form');
-  var messageField = document.getElementById('message');
-  var messagesList = document.getElementById('messages');
-  var socketStatus = document.getElementById('status');
-  var closeBtn = document.getElementById('close');
-  
-
-
-  // Cria um WebSocket
-  var socket = new WebSocket('ws://echo.websocket.org');
-
-
-  //Manipulador de erros ocorridos
-  socket.onerror = function(error) {
-    console.log('WebSocket erro: ' + error);
+function startWorker()
+{
+if(typeof(Worker) !== "undefined")
+{
+  if(typeof(w) == "undefined")
+    {
+    w = new Worker("app.js");
+    }
+  w.onmessage = function (event){
+    document.getElementById("result").innerHTML = event.data;
   };
+}
+else
+{
+document.getElementById("result").innerHTML="Desculpe seu navegador não suporta WebWorker...";
+}
+}
 
 
-  // Mensagem que aparece se for conectado ao servidor.
-  socket.onopen = function(event) {
-    socketStatus.innerHTML = 'Conectado a: ws://echo.websocket.org';
-    socketStatus.className = 'open';
-  };
+
+var i = 0;
+
+function timedCount()
+{
+    if(i != 100){
+        i = i + 1;
+        postMessage(i);
+        setTimeout("timedCount()",1000);
+    }
+}
+
+timedCount();
 
 
-  //Escreve mensagens na tela que foram enviadas pelo servidor.
-  socket.onmessage = function(event) {
-    var message = event.data;
-    messagesList.innerHTML += '<li class="received"><span>Recebido:</span>' +
-                               message + '</li>';
-  };
-
-
-  // Mensagem que aparece quando for desconectado do servidor.
-  socket.onclose = function(event) {
-    socketStatus.innerHTML = 'Desconectado do WebSocket';
-    socketStatus.className = 'closed';
-  };
-
-
-  //Envia a mensagem pelo formulário
-  form.onsubmit = function(e) {
-    e.preventDefault();
-
-    // Recupera a mensagem do TextArea
-    var message = messageField.value;
-
-    //  envia a mensagem através do WebSocket
-    socket.send(message);
-
-    // Adiciona a mensagem na lista
-    messagesList.innerHTML += '<li class="sent"><span>Enviado:</span>' + message +
-                              '</li>';
-
-    // Limpa o textarea
-    messageField.value = '';
-
-    return false;
-  };
-
-
-  // Fecha a conecção Websocket quando é clicado no botão.
-  closeBtn.onclick = function(e) {
-    e.preventDefault();
-
-    // Fecha WebSocket
-    socket.close();
-
-    return false;
-  };
-};
+function somarValores(){
+        var s1 = document.getElementById("text1").value;
+        var s2 = document.getElementById("text2").value;
+        var s3 = s1*s2;
+        document.getElementById("conta").innerHTML = s3;
+    }
