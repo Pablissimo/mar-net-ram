11d209209c84922b8a26b0336948b397592aa328
joseaugustoneto <joseneto.design@gmail.com>
2014-03-20 12:53:00 -0300

    Local Storage. Depois que escreve algo, ao atualizar o navegador ou fechar o texto volta como estava.

--- static/neto/app.js
+++ static/neto/app.js
@@ -1,42 +1,19 @@
-var w;
+window.onload = function(){
+    window.applicationCache.UPDATEREADY;
+};
 
-function startWorker()
-{
-if(typeof(Worker) !== "undefined")
-{
-  if(typeof(w) == "undefined")
-    {
-    w = new Worker("app.js");
-    }
-  w.onmessage = function (event){
-    document.getElementById("result").innerHTML = event.data;
-  };
-}
-else
-{
-document.getElementById("result").innerHTML="Desculpe seu navegador não suporta WebWorker...";
-}
-}
+ $(function() {
 
+	$("#lista").keypress(function() {
+	    localStorage.setItem('dados', $("#lista").html());
+	 });
 
+	if (localStorage.getItem('dados')) {
+	    $("#lista").html(localStorage.getItem('dados'));
+	}
 
-var i = 0;
-
-function timedCount()
-{
-    if(i != 100){
-        i = i + 1;
-        postMessage(i);
-        setTimeout("timedCount()",1000);
-    }
-}
-
-timedCount();
-
-
-function somarValores(){
-        var s1 = document.getElementById("text1").value;
-        var s2 = document.getElementById("text2").value;
-        var s3 = s1*s2;
-        document.getElementById("conta").innerHTML = s3;
-    }
+        $("#limpar").click(function() {
+	    localStorage.clear();
+	    window.location = window.location;
+	});	
+});
