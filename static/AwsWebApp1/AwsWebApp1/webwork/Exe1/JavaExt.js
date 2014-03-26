134d2c11e6b7e541268544f256cf7c1eef00f7da
Charles Danillo <danillocharles@gmail.com>
2014-03-26 11:56:54 -0300

    webworker

---
+++ static/AwsWebApp1/AwsWebApp1/webwork/Exe1/JavaExt.js
@@ -0,0 +1,27 @@
+﻿
+var worker; //Definindo a variavel 
+
+function startWorker()
+{
+    //Teste de compatibilidade do Browser para WebWorker
+    if(typeof(Worker)!=="undefined")
+    {
+        if(typeof(worker)=="undefined")
+        {
+            worker = new Worker("doWork.js"); // Instanciando o objeto Worker
+        }
+        worker.onmessage = function (event) //Evento para exibição de resultados
+        {
+            document.getElementById("result").innerHTML=event.data; //Onde serão impressos os resultados no HTML
+        };
+    }
+    else
+    {
+        document.getElementById("result").innerHTML="Sorry, your browser does not support Web Workers...";
+    }
+}
+//Função para finalizar o WebWorker
+function stopWorker()
+{ 
+    worker.terminate();
+}
