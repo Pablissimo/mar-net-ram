53de1d7802e090a9acd3efc786cd5645818bfdcc
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-03-26 07:29:57 -0300

    Projeto Local Storage

---
+++ static/AwsWebApp1/AwsWebApp1/Dados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AwsWebApp1
+{
+    public class DadosX
+    {
+        public string dados1 { get; set; }
+        public int dados2 { get; set; }
+    }
+}
---
+++ static/AwsWebApp1/AwsWebApp1/app.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    window.applicationCache.UPDATEREADY;
+};
+
+$(function () {
+
+    $("#lista").keypress(function () {
+        localStorage.setItem('dados', $("#lista").html());
+    });
+
+    if (localStorage.getItem('dados')) {
+        $("#lista").html(localStorage.getItem('dados'));
+    }
+
+    $("#limpar").click(function () {
+        localStorage.clear();
+        window.location = window.location;
+    });
+});
