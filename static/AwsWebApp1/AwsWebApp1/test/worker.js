bc1232891d14e87acc88115bb59bfb359f2dc2e2
Charles Danillo <danillocharles@gmail.com>
2014-03-27 10:27:32 -0300

    pasta teste

--- static/AwsWebApp1/AwsWebApp1/test/worker.js
+++ static/AwsWebApp1/AwsWebApp1/test/worker.js
@@ -1,5 +1,9 @@
-onmessage = function(event) {
-	var message = event.data;
-	var result = message + ' Testando...';
-	postMessage(result);
+var count = 0;
+onconnect = function (e) {
+    count += 1;
+    var port = e.ports[0];
+    port.postMessage('Hello World!' + count);
+    port.onmessage = function (e) {
+        port.postMessage('pong');
+    }
 }
