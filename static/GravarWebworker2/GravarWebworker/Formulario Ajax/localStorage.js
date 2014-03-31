a3dc006c62ba6efba464eca62b9e05d4b2ac335e
Charles Danillo <danillocharles@gmail.com>
2014-03-31 11:35:38 -0300

    gravando localstorage

---
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/localStorage.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $("#chave").blur(function () {
+        localStorage.setItem('chavelocal', $("#chave").val());
+    });
+    if (localStorage.getItem('chavelocal')) {
+        $("#chave").val(localStorage.getItem('chavelocal'));
+    }
+    $("#texto").blur(function () {
+        localStorage.setItem('textolocal', $("#texto").val());
+    });
+    if (localStorage.getItem('textolocal')) {
+        $("#texto").val(localStorage.getItem('textolocal'));
+    } $
+});
