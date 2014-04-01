b591f34399c44a76ab9b4513f5a547133727a004
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-01 08:30:39 -0300

    mystorage atualizado

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -1,12 +1,13 @@
 ﻿function gravar()
 {
 
+
     //localStorage.setItem('row_' + i, JSON.stringify(Objetos[i]));
     //var numero = 1;
    // eval("var chave" + numero);
 
 
-    var elemento_da_chave = eval($("#chave1")); // document.getElementById('chave') 
+    var elemento_da_chave = $("#chave1"); // document.getElementById('chave') 
     var chave1 = elemento_da_chave.val(); // elemento_da_chave.value
     localStorage.setItem('chavelocal', chave1);
 
@@ -25,7 +26,6 @@ function inilializar()
 {
     $("#chave1").blur(gravar);
     $("#texto1").blur(gravar);
-
 }
 
 $(function()
