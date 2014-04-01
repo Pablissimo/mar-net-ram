7ef81fd8891d663a7b06d381f1cb9fcec6b34b88
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-01 10:12:48 -0300

    LocalStorage Dinâmico Funcionando!

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -1,12 +1,19 @@
 ﻿function gravar()
 {
-    var cha = $(".chave").val();
-    alert(cha);
+    var dados_do_form_como_array = ($('#form').serializeArray());
+    $.each(dados_do_form_como_array,
+        function (nome, valor)
+        {
+            var nomeLocalSotarage = valor.name;
+            var valorLocalSotarage = valor.value;
+            localStorage.setItem(nomeLocalSotarage, valorLocalSotarage);
+        }
+    );
+    
+    var dados_do_form_serializado = JSON.stringify(dados_do_form_como_array);
 
-    //localStorage.setItem('row_' + i, JSON.stringify(Objetos[i]));
-    //var numero = 1;
-   // eval("var chave" + numero);
 
+    /*postMessage(dados_do_form_serializado);
 
     var elemento_da_chave = $("#chave1"); // document.getElementById('chave') 
     var chave1 = elemento_da_chave.val(); // elemento_da_chave.value
@@ -14,7 +21,7 @@
 
     var elemento_da_chave = $("#texto1"); // document.getElementById('chave') 
     var texto1 = elemento_da_chave.val(); // elemento_da_chave.value
-    localStorage.setItem('textolocal', texto1);
+    localStorage.setItem('textolocal', texto1);*/
    
 }
 
@@ -25,9 +32,8 @@ function carregar()
 
 function inilializar()
 {
-    $("#chave1").blur(gravar);
-    $("#texto1").blur(gravar);
-
+    $(".chave").blur(gravar);
+    $(".texto").blur(gravar);
 }
 
 $(function()
