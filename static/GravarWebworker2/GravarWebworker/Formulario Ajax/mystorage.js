627ed90aa7f38cddf1948d92b23374e813fe4c02
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-01 07:30:40 -0300

    funcoes

---
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -0,0 +1,46 @@
+﻿
+function gravar()
+{
+    var elemento_da_chave = $("#chave"); // document.getElementById('chave') 
+    var chave = elemento_da_chave.val(); // elemento_da_chave.value
+    localStorage.setItem('chavelocal', chave);
+    return
+}
+
+function carregar()
+{
+
+}
+
+function inilializar()
+{
+    $("#chave").blur(gravar);
+}
+
+$(function()
+{
+    inilializar();
+})
+
+
+
+var chave, texto;
+
+
+function () {
+    
+    if (localStorage.getItem('chavelocal')) {
+        $("#chave").val(localStorage.getItem('chavelocal'));
+    }
+    $("#texto").blur(
+        function () {
+            texto = $("texto").val();
+            localStorage.setItem('textolocal', texto);
+    });
+    if (localStorage.getItem('textolocal')) {
+        $("#texto").val(localStorage.getItem('textolocal'));
+    } $
+});
+
+var x = { chave: chave, texto: texto };
+alert(x); 
