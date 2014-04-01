2b395871cb8570fa97e1ae74739e60f3f4cac03e
Charles Danillo <danillocharles@gmail.com>
2014-04-01 11:51:36 -0300

    commit

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -1,24 +1,67 @@
 ﻿function gravar()
 {
-    var elemento_da_chave = $("#chave1"); // document.getElementById('chave') 
-    var chave1 = elemento_da_chave.val(); // elemento_da_chave.value
-    localStorage.setItem('chavelocal', chave1);
+    //var dados_do_form_como_array = $('#form').size();
 
+<<<<<<< ours
     var elemento_da_chave = $("#texto1"); // document.getElementById('chave') 
     var texto1 = elemento_da_chave.val(); // elemento_da_chave.value
     localStorage.setItem('textolocal', texto1); 
+=======
+    for (var i = 1; i <= 3 ; i++)
+    {
+        var dado = $("#dado" + i).val();
+        localStorage.setItem(i, dado);
+    }
+    
+    /*var dados_do_form_como_array = ($('#form').serializeArray()); //Serializando o Array do Formulário com JSON, ex.: { name : value } 
+    
+
+    //Foreach do JQUERY de Atribuição.
+    $.each(dados_do_form_como_array, 
+        function (nome, valor) // Onde nome = posição do array(objeto) e valor é o valor da posição do array ( { name : value} ) 
+        {
+            var nomeLocalSotarage = valor.name; //Pegaando apenas o Título do atributo JSON, ex: { name }
+            var valorLocalSotarage = valor.value; //Pegaando apenas o Valor do atributo JSON, ex: { value }
+            localStorage.setItem(nomeLocalSotarage, valorLocalSotarage); //Setando o LocalStorage.
+        }
+    );*/
+
+    //Preparar para Gravar com WEBWORKER
+    //var dados_do_form_serializado = JSON.stringify(dados_do_form_como_array);
+   
+>>>>>>> theirs
 }
 
 function carregar()
 {
 
+    for (var i = 1; i <= 3 ; i++)
+    {
+        var dado = localStorage.getItem(i);
+        $("#dado" + i).val(dado);
+    }
+
+    /*
+    var dados_do_form_como_array = ($('#form').serializeArray()); //Serializando o Array do Formulário com JSON, ex.: { name : value } 
+    //Foreach do JQUERY de Carregamento.
+    $.each(dados_do_form_como_array, 
+        function (nome, valor) {
+
+            var nomeLocalSotarage = valor.name;
+            var valorLocalSotarage = valor.value;
+
+            if (localStorage.getItem(nomeLocalSotarage)) {
+                eval('$("#' + nomeLocalSotarage + '")').val(localStorage.getItem(nomeLocalSotarage));//$("#chave1").val(localStorage.getItem(nomeLocalSotarage));
+            }
+        }
+    )*/
 }
 
 function inilializar()
 {
-    $("#chave1").blur(gravar);
-    $("#texto1").blur(gravar);
-
+    $("#form").ready(carregar);
+    $(".chave").blur(gravar);
+    $(".dado").blur(gravar);
 }
 
 $(function()
