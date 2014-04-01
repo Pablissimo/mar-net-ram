6d915d6fba1d8506808b678ec24f72faaa3968b0
Charles Danillo <danillocharles@gmail.com>
2014-04-01 08:33:56 -0300

    commit rotina

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -1,19 +1,13 @@
 ﻿function gravar()
 {
-
-    //localStorage.setItem('row_' + i, JSON.stringify(Objetos[i]));
-    //var numero = 1;
-   // eval("var chave" + numero);
-
-
-    var elemento_da_chave = eval($("#chave1")); // document.getElementById('chave') 
+    var elemento_da_chave = $("#chave1"); // document.getElementById('chave') 
     var chave1 = elemento_da_chave.val(); // elemento_da_chave.value
     localStorage.setItem('chavelocal', chave1);
 
     var elemento_da_chave = $("#texto1"); // document.getElementById('chave') 
     var texto1 = elemento_da_chave.val(); // elemento_da_chave.value
     localStorage.setItem('textolocal', texto1);
-    //numero++;
+   
 }
 
 function carregar()
@@ -34,24 +28,3 @@ $(function()
 })
 
 
-
-//var chave, texto;
-
-
-/*function () {
-    
-    if (localStorage.getItem('chavelocal')) {
-        $("#chave").val(localStorage.getItem('chavelocal'));
-    }
-    $("#texto").blur(
-        function () {
-            texto = $("texto").val();
-            localStorage.setItem('textolocal', texto);
-    });
-    if (localStorage.getItem('textolocal')) {
-        $("#texto").val(localStorage.getItem('textolocal'));
-    } $
-});
-
-var x = { chave1: chave, texto2: texto };
-alert(x); */
