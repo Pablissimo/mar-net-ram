b36fb0e458d435133c77b09f5fa035da2e3d77f9
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-01 11:55:32 -0300

    Gravar Clean.

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/mystorage.js
@@ -1,54 +1,23 @@
 ﻿function gravar()
 {
-    //var dados_do_form_como_array = $('#form').size();
-
-    for (var i = 1; i <= 3 ; i++)
+    var tamanhoArrayForm = $('#form').serializeArray().length;
+    
+    for (var i = 1; i <= tamanhoArrayForm ; i++)
     {
         var dado = $("#dado" + i).val();
         localStorage.setItem(i, dado);
     }
-    
-    /*var dados_do_form_como_array = ($('#form').serializeArray()); //Serializando o Array do Formulário com JSON, ex.: { name : value } 
-    
-
-    //Foreach do JQUERY de Atribuição.
-    $.each(dados_do_form_como_array, 
-        function (nome, valor) // Onde nome = posição do array(objeto) e valor é o valor da posição do array ( { name : value} ) 
-        {
-            var nomeLocalSotarage = valor.name; //Pegaando apenas o Título do atributo JSON, ex: { name }
-            var valorLocalSotarage = valor.value; //Pegaando apenas o Valor do atributo JSON, ex: { value }
-            localStorage.setItem(nomeLocalSotarage, valorLocalSotarage); //Setando o LocalStorage.
-        }
-    );*/
-
-    //Preparar para Gravar com WEBWORKER
-    //var dados_do_form_serializado = JSON.stringify(dados_do_form_como_array);
-   
 }
 
 function carregar()
 {
+    var tamanhoArrayForm = $('#form').serializeArray().length;
 
-    for (var i = 1; i <= 3 ; i++)
+    for (var i = 1; i <= tamanhoArrayForm ; i++)
     {
         var dado = localStorage.getItem(i);
         $("#dado" + i).val(dado);
     }
-
-    /*
-    var dados_do_form_como_array = ($('#form').serializeArray()); //Serializando o Array do Formulário com JSON, ex.: { name : value } 
-    //Foreach do JQUERY de Carregamento.
-    $.each(dados_do_form_como_array, 
-        function (nome, valor) {
-
-            var nomeLocalSotarage = valor.name;
-            var valorLocalSotarage = valor.value;
-
-            if (localStorage.getItem(nomeLocalSotarage)) {
-                eval('$("#' + nomeLocalSotarage + '")').val(localStorage.getItem(nomeLocalSotarage));//$("#chave1").val(localStorage.getItem(nomeLocalSotarage));
-            }
-        }
-    )*/
 }
 
 function inilializar()
