6ca15e912e050aea009f195e5ef6065afcbfcd58
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-02 08:41:15 -0300

    worker com preperação de dados

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/Worker.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/Worker.js
@@ -1,4 +1,18 @@
 ﻿onmessage = function (e) {
-    postMessage('chegou');
+    var dados_string = e.data;
+
+    var chave = dados_string.substring(0,1);
+    var dado = dados_string.substring(2);
+    var req = new XMLHttpRequest();
+    req.open('GET', 'PersistirDynamoDB.ashx?action=SavePerson', false);
+
+    req.setRequestHeader('Content-Type', 'application/json;  charset=utf-8');
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            self.postMessage({ 'Error': 'No', 'Message': 'Save Successful' });
+        }
+    }
+    req.send(dado);
+    req.send(chave);
 
 }
