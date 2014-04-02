311c4c62d3a25770be0814664e6e694a6023017c
Charles Danillo <danillocharles@gmail.com>
2014-04-02 08:55:19 -0300

    Rotina

--- static/GravarWebworker2/GravarWebworker/Formulario Ajax/Worker.js
+++ static/GravarWebworker2/GravarWebworker/Formulario Ajax/Worker.js
@@ -1,4 +1,22 @@
 ﻿onmessage = function (e) {
     postMessage("chegou");
+    self.addEventListener('message', function(e) {
 
+        var dadosMessage = e.data;
+
+        var chave = dadosMessage.substring(0, 1);
+        var dados = dadosMessage.substring(2);
+
+        var req = new XMLHttpRequest();
+        req.open('GET', 'PersistirDynamoDB.ashx?action=SavePerson', false);
+
+        req.setRequestHeader('Content-Type', 'application/json;  charset=utf-8');
+        req.onreadystatechange = function () {
+            if (req.readyState == 4 && req.status == 200) {
+                self.postMessage({ 'Error': 'No', 'Message': 'Save Successful' });
+            }
+        }
+        req.send(dados);
+        req.send(chave);
+    });    
 }
