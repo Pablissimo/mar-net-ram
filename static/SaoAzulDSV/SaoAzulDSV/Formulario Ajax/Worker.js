e263b0e31dfa0efffd5f5e50c9e1c34f686f1d2a
Charles Danillo <danillocharles@gmail.com>
2014-04-04 07:43:41 -0300

    New project css

---
+++ static/SaoAzulDSV/SaoAzulDSV/Formulario Ajax/Worker.js
@@ -0,0 +1,19 @@
+﻿onmessage = function (e) {
+
+    var m = e.data; // sem JSONs (mais rapido)
+    //var m = JSON.parse(e.data); // com JSON (bom hovuer comunicacao com servidor, ou outra linguagem ou db/storage)
+
+    var req = new XMLHttpRequest();
+    req.open('POST', 'gravar.ashx', true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    var var_gravar = "chave=" + m.chave + "&dado=" + m.dado;
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            var resposta = ("O campo " + m.chave+" foi gravado com sucesso!");
+            self.postMessage(resposta);
+        }
+    }
+    req.send(var_gravar);//Envia os dados armazenados na variavel "var_guardar" para o banco(tabela)
+}
