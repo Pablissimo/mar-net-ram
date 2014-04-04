4217473795853939974b1734a85b5230511974b2
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-04 11:48:18 -0300

    Alteração Temas

--- static/bpmos/bpmos/formManipulation.js
+++ static/bpmos/bpmos/formManipulation.js
@@ -1,1 +1,44 @@
-﻿$('.verde').addClass('defaultActive');
+﻿$(function()
+{
+    $('.default').ready(temaDefault);
+
+    $('.verde').click(temaVerde);
+    $('.roxo').click(temaRoxo);
+    $('.vermelho').click(temaVermelho);
+    $('.default').click(temaDefault);
+})
+
+
+
+function temaRoxo()
+{
+    removeTema();
+    $('.roxo').addClass('roxoActive');
+}
+
+function temaVerde()
+{
+    removeTema();
+    $('.verde').addClass('verdeActive');
+}
+
+function temaVermelho()
+{
+    removeTema();
+    $('.vermelho').addClass('vermelhoActive');
+}
+
+
+function temaDefault()
+{
+    removeTema();
+    $('.default').addClass('defaultActive');
+}
+
+function removeTema()
+{
+    $('.verde').removeClass('verdeActive');
+    $('.roxo').removeClass('roxoActive');
+    $('.vermelho').removeClass('vermelhoActive');
+    $('.default').removeClass('defaultActive');
+}
