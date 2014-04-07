c4e8e5e5b545ea75c96e2f0bc49d11c209973e52
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-07 11:59:37 -0300

    Pagina com estilos, só não está trocando o estilo quando da um click.

---
+++ static/bpmos/bpmos/csschange/manipuladorCSS.js
@@ -0,0 +1,9 @@
+﻿$('#estilo').click(function() {
+    $("#style1").removeAttr("disabled");
+    $("#style2").Attr("disabled", "disabled");
+})
+
+$('#estilo2').click(function() {
+    $("#style2").removeAttr("disabled");
+    $("#style1").Attr("disabled", "disabled");
+})
