a4bb6d985a3bf40c731fe55dba194dcabe987147
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-07 07:06:23 -0300

    Rotina

--- static/bpmos/bpmos/formManipulation.js
+++ static/bpmos/bpmos/formManipulation.js
@@ -7,34 +7,26 @@
     $('.vermelho').click(temaVermelho);
     $('.default').click(temaDefault);
 })
-
-
-
 function temaRoxo()
 {
     removeTema();
     $('.roxo').addClass('roxoActive');
 }
-
 function temaVerde()
 {
     removeTema();
     $('.verde').addClass('verdeActive');
 }
-
 function temaVermelho()
 {
     removeTema();
     $('.vermelho').addClass('vermelhoActive');
 }
-
-
 function temaDefault()
 {
     removeTema();
     $('.default').addClass('defaultActive');
 }
-
 function removeTema()
 {
     $('.verde').removeClass('verdeActive');
