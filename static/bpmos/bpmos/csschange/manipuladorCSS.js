6ff86397c008063e18e7ea51a6a19366d6a83d2d
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-07 12:02:35 -0300

    Uktimo Arquivo De estilo.

--- static/bpmos/bpmos/csschange/manipuladorCSS.js
+++ static/bpmos/bpmos/csschange/manipuladorCSS.js
@@ -1,9 +1,19 @@
-﻿$('#estilo').click(function() {
+﻿$("#estilo").click(function() {
     $("#style1").removeAttr("disabled");
     $("#style2").Attr("disabled", "disabled");
 })
 
-$('#estilo2').click(function() {
+$("#estilo2").click(function() {
     $("#style2").removeAttr("disabled");
     $("#style1").Attr("disabled", "disabled");
 })
+
+$("#estilo").click(function () {
+    $("link[href*=estilo2.css]").attr("disabled", "disabled");
+    $("link[href*=estilo.css]").removeAttr("disabled");
+});
+
+$("#estilo2").click(function () {
+    $("link[href*=estilo1.css]").attr("disabled", "disabled");
+    $("link[href*=estilo2.css]").removeAttr("disabled");
+});
