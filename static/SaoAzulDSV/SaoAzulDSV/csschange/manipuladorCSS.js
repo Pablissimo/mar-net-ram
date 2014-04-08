ee691a4947da438ea0b4b1d1f9930a2e974d2c8e
Charles Danillo <danillocharles@gmail.com>
2014-04-08 10:53:44 -0300

    Manipulador CSS Final (Remove e ADD CSS)

--- static/SaoAzulDSV/SaoAzulDSV/csschange/manipuladorCSS.js
+++ static/SaoAzulDSV/SaoAzulDSV/csschange/manipuladorCSS.js
@@ -1,72 +1,19 @@
-﻿//$(function () { //metodo de disabilitar
-//    $("#style2").attr("disabled", true);
+﻿$(function () {  //metodo de disabilitar CSS da pagina HTML
 
+    var estilo_inicial = localStorage.getItem("estilo");
+    if (!estilo_inicial)
+        estilo_inicial = 'estilo';
+    mudar_estilo(estilo_inicial);
 
-//    $("#estilo").click(estilo1);
-//    $("#estilo2").click(estilo2);
-
-//})
-
-//function estilo1() {
-//    $("#style2").attr("disabled", true);
-//    $("#style1").removeAttr("disabled");
-//}
-
-//function estilo2() {
-//    $("#style1").attr("disabled", true);
-//    $("#style2").removeAttr("disabled");
-//}
-
-
-
-
-//$(function () { //metodo que remove a linha de estilo através da função removejscssfile
-//    removejscssfile("estilo2.css", "css"); //remove all occurences of "somescript.js" on page
-
-
-//    $("#estilo").click(estilo1);
-//    $("#estilo2").click(estilo2);
-
-//})
-
-//function estilo1() {
-
-//    removejscssfile("estilo2.css", "css"); //remove all occurences of "somescript.js" on page
-//    $("head").append("<link rel='stylesheet' type='text/css' href='estilo.css' id='style1'>");
-//}
-
-//function estilo2() {
-//    removejscssfile("estilo.css", "css"); //remove all occurences of "somescript.js" on page
-//    $("head").append("<link rel='stylesheet' type='text/css' href='estilo2.css' id='style1'>");
-//}
-
-
-//function removejscssfile(filename, filetype) {
-//    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none" //determine element type to create nodelist from
-//    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none" //determine corresponding attribute to test for
-//    var allsuspects = document.getElementsByTagName(targetelement)
-//    for (var i = allsuspects.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
-//        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
-//            allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-//    }
-//}
-
-
-$(function () {  //metodo de disabilitar
-    $("#style2").attr("disabled", true);
-
-
-    $("#estilo").click(estilo1);
-    $("#estilo2").click(estilo2);
+    $("#estilo").click(function () { mudar_estilo("estilo"); });
+    $("#estilo2").click(function () { mudar_estilo("estilo2"); });
 
 })
 
-function estilo1() {
-    $("#style2").attr("disabled", true);
-    $("#style1").removeAttr("disabled");
-}
-
-function estilo2() {
-    $("#style1").attr("disabled", true);
-    $("#style2").removeAttr("disabled");
+function mudar_estilo(nome) {
+    var estilo = $("style");
+    if (estilo)
+      estilo.remove(estilo);
+    $("head").append("<link rel='stylesheet' type='text/css' href='" + nome + ".css' id='style'>");
+    localStorage.setItem("estilo", nome);
 }
