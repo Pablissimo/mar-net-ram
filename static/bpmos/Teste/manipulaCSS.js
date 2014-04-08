3da3e5df7e7cdcf1dcab12361829cb3111bc153a
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-04-08 09:47:02 -0300

    Commit de teste

--- static/bpmos/Teste/manipulaCSS.js
+++ static/bpmos/Teste/manipulaCSS.js
@@ -1,19 +1,37 @@
 ﻿$(function ()
 {
-    $("#style2").attr("disabled", true);
+    //$("#style2").attr("disabled", true);
 
     $("#estilo").click(chamaEstilo);
     $("#estilo2").click(chamaEstilo2);
+
+    removejscssfile("somescript.js", "js") //remove all occurences of "somescript.js" on page
+    removejscssfile("somestyle.css", "css") //remove all occurences "somestyle.css" on page
+
 });
 
 function chamaEstilo()
 {
-    $("#style2").attr("disabled", true);
-    $("#style1").removeAttr("disabled");
+    //$("#style2").attr("disabled", true);
+    //$("#style1").removeAttr("disabled");
+
 }
 
 function chamaEstilo2()
 {
-    $("#style1").attr("disabled", true);
-    $("#style2").removeAttr("disabled");
+    //$("#style1").attr("disabled", true);
+    //$("#style2").removeAttr("disabled");
+
 }
+
+
+function removejscssfile(filename, filetype) {
+    var targetelement = (filetype == "js") ? "script" : (filetype == "css") ? "link" : "none" //determine element type to create nodelist from
+    var targetattr = (filetype == "js") ? "src" : (filetype == "css") ? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects = document.getElementsByTagName(targetelement)
+    for (var i = allsuspects.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
+        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) != null && allsuspects[i].getAttribute(targetattr).indexOf(filename) != -1)
+            allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+}
+
---
+++ static/bpmos/bpmos/manipuladorCSS.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    // $("link[href=estiloAlternativo.css]").attr("disabled");
+    $("#estiloAlternativo").attr("disabled");
+    $("link[href=estilosensitive.css]").attr("disabled");
+
+    $('.alternativo').click(trocar_tema_Alternativo);
+    $('.sensitive').click(trocar_tema_Alternativo);
+    $('.padrao').click(trocar_tema_padrao);
+});
+
+function trocar_tema_Alternativo() {
+
+    //$("head").append("<link rel='stylesheet' type='text/css' href='estiloAlternativo.css'>")
+    //$('body').css({
+    //    background: "#fff"
+    //});
+    $("link[href=estiloAlternativo.css]").removeAttr("disabled");
+
+}
+
+function trocar_tema_sensitive() {
+    //$("head").append("<link rel='stylesheet' type='text/css' href='estilosensitive.css'>")
+    //$('body').css({
+    //    background: "#ffb7f5"
+    //});
+}
+function trocar_tema_padrao() {
+    location.reload(true);
+}
