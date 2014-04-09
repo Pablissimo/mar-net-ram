32ba4f5efce3d38efc9d8e9c21e45e161ddea9b5
joseaugustoneto <joseneto.design@gmail.com>
2014-04-09 00:24:14 -0300

    Comite de Add Input

---
+++ static/SaoAzulDSV/SaoAzulDSV/Neto/adiciona_input.js
@@ -0,0 +1,31 @@
+﻿//$(function (e) {
+//    if (e.wich == 13 || e.keyCode == 13)
+//        if (!i) {
+//            var i = 0;
+//            $("#div_input").append("<input id='dado" + i + "' class='dado' name='dado" + i + "' placeholder='Escreva aqui' />");
+//            return;
+//        }
+//        //else
+//        //    i++;
+//        //    $("div_input").append("<input id='dado" + i + "' class='dado' name='dado" + i + "' placeholder='Escreva aqui' />");
+   
+//})
+$(window).load(function () {
+    for (i = 1; i < localStorage.getItem("contador") ; i++) {
+        $("#div_input").append("<input id='dado" + i + "' class='dado' name='dado" + i + "' placeholder='Escreva aqui' /><br>");
+    }
+});
+
+$(function () {
+    var contador = localStorage.getItem("contador");
+    if (!contador)
+        contador = 1;
+    add(contador);
+
+    $("#btn").click(function () {add(contador++)});
+})
+
+function add(contar) {
+    $("#div_input").append("<input id='dado" + contar + "' class='dado' name='dado" + contar + "' placeholder='Escreva aqui' /><br>");
+    localStorage.setItem("contador", contar);
+}
