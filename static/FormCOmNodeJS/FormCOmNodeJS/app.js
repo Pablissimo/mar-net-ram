a8a9cd8880043a7bddbe49ba80cdbf29ba14befb
danillo <danillocharles@gmaiil.com>
2014-04-24 00:15:19 -0300

    Criação e estruturação.

--- static/FormCOmNodeJS/FormCOmNodeJS/app.js
+++ static/FormCOmNodeJS/FormCOmNodeJS/app.js
@@ -1,9 +1,43 @@
-﻿function Cliente (){};
+﻿
+var qdb = require("./qualiomdb.js")
 
-var cliente = new Cliente();
+var ClasseAplicacao = function () {
 
-cliente.id= "1";
-client.nome = "Ramon";
+    var self = this;
+
+    self.teste_caso_1 = function () {
+        var A = new qdb.QualiomDB('A');
+        var B = new qdb.QualiomDB('B');
+
+        //1
+        A.adicionar('um');
+        A.adicionar('dois');
+        A.adicionar('tres');
+
+        //2
+        B.adicionar('um');
+        B.adicionar('dois');
+        B.adicionar('tres');
+        B.adicionar('quatro');
 
 
-console.log(cliente);
+        //3
+        A.deletar; ('A1');
+        A.alterar('A3', 'três');
+
+        //4
+        B.alterar('B3', 'três');
+
+        // sync
+        A.sync();
+        B.sync();
+
+        A.listadados();
+        B.listadados();
+
+    };
+};
+
+var app = new ClasseAplicacao();
+
+app.teste_caso_1();
---
+++ static/FormCOmNodeJS/FormCOmNodeJS/clienteBD.js
@@ -0,0 +1,33 @@
+﻿exports.QualiomDB = function (banco) {
+    var self = this;
+    self.db = new Array();
+    self.nome_banco = banco;
+
+    self.contador = 0;
+
+    self.gerarchave = function (dado) {
+        self.contador++;
+        return self.nome_banco + self.contador;
+    }
+
+    self.adicionar = function (dado) {
+        var chave = self.gerarchave();
+        self.db.push({ chave: chave, dado: dado });
+    };
+
+
+    self.deletar = function () {
+
+    }
+
+    self.alterar = function () {
+
+    }
+
+    self.listadados = function (a) {
+        for (i = 0; i <= db.length; i++) {
+            console.log(db[i]);
+        }
+    }
+
+};
