3f335c81e48154ffe35f376ba4bc263accaca9d1
danillo <danillocharles@gmaiil.com>
2014-04-25 01:46:33 -0300

    commit

---
+++ static/AppNodejsConsole/AppNodejsConsole/ReplicacaoTests.js
@@ -0,0 +1,112 @@
+﻿    // UnitTest.js 
+var assert = require('assert');
+var qdb = require('./qualiomdb.js');
+
+exports['Caso 1'] = function (test) {
+
+
+    var A = new qdb.QualiomDB('A');
+    var B = new qdb.QualiomDB('B');
+
+    //1
+    A.adicionar('um');
+    A.adicionar('dois');
+    A.adicionar('tres');
+
+    //2
+    B.adicionar('um');
+    B.adicionar('dois');
+    B.adicionar('tres');
+    B.adicionar('quatro');
+
+
+    //3
+    A.deletar(0);
+    A.alterar(2, 'três');
+
+    //4
+    B.alterar(2, 'três');
+
+    // sync
+    A.sync();
+    B.sync();
+    A.sync();
+
+
+    var esperado_um = [{ chave: 'B1', dado: 'um' }];
+    assert.deepEqual(esperado_um, A.pesquisar('um'), "dado UM banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('um'), "dado UM banco B");
+
+    var esperado_dois = [{ chave: 'A2', dado: 'dois' }, { chave: 'B2', dado: 'dois' }];
+    assert.deepEqual(esperado_um, A.pesquisar('dois'), "dado DOIS banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('dois'), "dado DOIS banco B");
+
+    var esperado_tres = [];
+    assert.deepEqual(esperado_um, A.pesquisar('tres'), "dado TRES banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('tres'), "dado TRES banco B");
+
+    var esperado_tres_acentuado = [{ chave: 'A3', dado: 'três' }, { chave: 'B2', dado: 'três' }];
+    assert.deepEqual(esperado_um, A.pesquisar('três'), "dado TRÊS banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('três'), "dado TRÊS banco B");
+
+    var esperado_quatro = [{ chave: 'B4', dado: 'quatro' }];
+    assert.deepEqual(esperado_um, A.pesquisar('quatro'), "dado QUATRO banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('quatro'), "dado QUATRO banco B");
+
+}
+
+
+exports['Caso 3'] = function (test) {
+
+
+    var A = new qdb.QualiomDB('A');
+    var B = new qdb.QualiomDB('B');
+
+    //1
+    A.adicionar('um');
+
+    //2 sync
+    A.sync();
+    B.sync();
+    A.sync();
+
+    // 3
+    B.adicionar('dois');
+    // 4
+    B.deletar('A1');
+
+    // 5
+    A.deletar('A1');
+    A.adicionar('dois');
+
+    // caiu internet no B
+
+
+    var esperado_um = [];
+
+    assert.deepEqual(esperado_um, A.pesquisar('um'), "dado UM banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('um'), "dado UM banco B");
+
+
+    var esperado_dois_A = [{ chave: 'A2', dado: 'dois' }];
+    var esperado_doisBA = [{ chave: 'B1', dado: 'dois' }];
+
+    assert.deepEqual(esperado_dois_A, A.pesquisar('dois'), "dado DOIS banco A");
+    assert.deepEqual(esperado_doisBA, B.pesquisar('dois'), "dado DOIS banco B");
+
+
+    // sync
+    A.sync();
+    B.sync();
+    A.sync();
+
+
+    var esperado_um = [];
+    assert.deepEqual(esperado_um, A.pesquisar('um'), "dado UM banco A");
+    assert.deepEqual(esperado_um, B.pesquisar('um'), "dado UM banco B");
+
+    var esperado_dois = [{ chave: 'A2', dado: 'dois' }, { chave: 'B1', dado: 'dois' }];
+    assert.deepEqual(esperado_dois, A.pesquisar('dois'), "dado DOIS banco A");
+    assert.deepEqual(esperado_dois, B.pesquisar('dois'), "dado DOIS banco B");
+
+}
---
+++ static/AppNodejsConsole/AppNodejsConsole/qualiomdb.js
@@ -0,0 +1,128 @@
+﻿exports.QualiomDB = function (banco)
+{
+    var self = this;
+    self.db = new Array();
+    self.nome_banco = banco;
+
+    self.contador = 0;
+
+    self.gerarchave = function (dado)
+    {
+        self.contador++;
+        return self.nome_banco + self.contador;
+    }
+
+    self.adicionar = function (dado)
+    {
+        var chave = self.gerarchave();
+        self.db.push({ chave: chave, dado: dado });
+    };
+
+    self.deletar = function (chave)
+    {
+        for (var index in self.db) {
+            var registro = self.db[index];
+            if (registro.chave == chave) {
+                delete registro.dado;
+                delete registro.chave;
+            }
+        }
+    }
+
+    self.alterar = function (chave, dado)
+    {
+        for (var index in self.db)
+        {
+            var registro = self.db[index];
+            if (registro.chave == chave)
+            {
+                registro.dado = dado;
+            }
+        }
+    }
+
+    self.pesquisar = function (dado)
+    {
+        var retorno = new Array();
+        for (var index in self.db)
+        {
+            var registro = self.db[index];
+            if (registro.dado == dado)
+            {
+                retorno.push(registro);
+            }
+        }
+        retorno.sort(function (a, b)
+        {
+            if (a.chave == a.b.chave)
+                return 0;
+            if (a.chave > a.b.chave)
+                return 1;
+            return -1;
+        });
+        return retorno;
+    }
+
+    self.listadados = function ()
+    {
+        return self.db;
+    }
+
+    self.sync = function ()
+    {
+        var bCent = bancoCentral.db;
+        var bLocal = self.db;
+
+        
+        if (bCent.length == 0 && bLocal.length != 0)
+        {
+            bCent = bLocal;
+        }
+        else
+        {
+            for (var i = 0; i <= bLocal.length; i++)
+            {
+                for (var j = 0; j <= bCent.length; j++)
+                {
+                    if(bLocal[i].chave != bCent[j].chave)
+                    {
+                        bCent.push(bLocal[i]);
+                    }
+                }
+            }
+        }
+
+        return bCent;
+    }
+
+};
+
+var bancoCentral = new exports.QualiomDB("Central");
+
+    /*
+    
+                   
+        var igual = false;
+        for (var index in banco)
+        {
+            var registro = banco[index];
+
+
+            for (var i = 0; i < self.db.length; i++)
+            {
+                if (self.db[i].chave == registro.chave)
+                {
+                    igual = true;
+                }
+            }
+
+            if (igual == false)
+            {
+                self.db.push(registro);
+            }
+            igual = false;
+        }
+    
+    
+    
+    */
