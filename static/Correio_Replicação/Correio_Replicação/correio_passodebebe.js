66a076b62ce03c12afde7b3dfe25c4a8bba5afa1
Charles Danillo <danillocharles@gmail.com>
2014-04-28 11:54:29 -0300

    Correio Passo de Bebe

---
+++ static/Correio_Replicação/Correio_Replicação/correio_passodebebe.js
@@ -0,0 +1,29 @@
+﻿exports.Correio = (function () {
+    function Correio() {
+    }
+    var listaMensagem = new Array();
+
+
+    Correio.prototype.enviaMsg = function (remetente, destinatario, mensagem) {
+        listaMensagem.push({ remetente: remetente, destinatario: destinatario, mensagem: mensagem });
+    };
+    
+    Correio.prototype.listaMsg = function (destinatario) {
+        var resultado = new Array();
+        var outrosdestinarios = new Array();
+        
+        for (var index in listaMensagem) {
+            var msg=listaMensagem[index];
+            if (msg.destinatario == destinatario)
+                resultado.push(msg);
+            else
+                outrosdestinarios.push(msg);
+        }
+
+        listaMensagem = outrosdestinarios;
+        return resultado;
+    };
+
+
+    return Correio;
+})();
