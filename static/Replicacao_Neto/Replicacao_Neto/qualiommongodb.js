2bea10511a6c916661f2d073a0ce6ed4b192580e
Charles Danillo <danillocharles@gmail.com>
2014-04-29 08:31:27 -0300

    Commitando mudanças no server mongoDB.

--- static/Replicacao_Neto/Replicacao_Neto/qualiommongodb.js
+++ static/Replicacao_Neto/Replicacao_Neto/qualiommongodb.js
@@ -1,13 +1,10 @@
-﻿var mongodb = require('mongodb');
+﻿    // https://github.com/openshift-quickstart/openshift-mongo-node-express-example/blob/master/server.js  
 
-exports.QualiomDB = function (banco) {
+var mongodb = require('mongodb');
+
+var QualiomDB = function () {
     var self = this;
-    self.db = new Array();//array do banco
-    self.log = new Array();//array do log/historico
-    self.nome_banco = banco;//nome do banco de dados para geracao da chave
-    self.indexLog = 0;//qual o ultimo item enviado para o banco matriz
-    self.contador = 0;// contador da chave
-
+    var db = null;
 
     self.conectar = function (banco, callback) {
 
@@ -30,6 +27,8 @@ exports.QualiomDB = function (banco) {
         });
     };
 
+    self.contador = 0;
+
     self.gerarchave = function (dado, callback) {
         self.contador++;
         return self.nome_banco + self.contador;
@@ -42,146 +41,6 @@ exports.QualiomDB = function (banco) {
          callback);
     };
 
-    //self.gerarchave = function (dado) {//gerador de chaves
-    //    self.contador++;
-    //    return self.nome_banco + self.contador;
-    //}
-
-    //self.adicionar = function (dado) {// adiciona um dado no proprio banco
-    //    var chave = self.gerarchave();
-    //    self.db.push({ chave: chave, dado: dado });//grava o dado no banco
-    //    self.log.push({ operacao: 'adicionar', chave: chave, dado: dado }); //historia
-    //};
-
-    self.deletar = function (chave) {//deleta um dado no proprio banco
-        for (var index in self.db) {
-            var registro = self.db[index];
-            if (registro.chave == chave) {//compara a chave do registro com a chave que vai ser excluida
-                delete self.db[index];//exclui
-                self.log.push({ operacao: 'deletar', chave: chave });//historia
-            }
-        }
-    }
-
-    self.alterar = function (chave, dado) {//altera um dado no proprio banco
-        for (var index in self.db) {
-            var registro = self.db[index];
-            if (registro.chave == chave) {//comparação de chaves
-                registro.dado = dado;// altera dado
-                self.log.push({ operacao: 'alterar', chave: chave, dado: dado });//historia
-            }
-        }
-    }
-
-
-
-
-    self.pesquisar = function (dado) {//pesquisa o dado no banco
-        var retorno = new Array();
-        for (var index in self.db) {
-            var registro = self.db[index];
-            if (registro.dado == dado) {
-                retorno.push(registro);
-            }
-        }
-        retorno.sort(function (a, b) {//ordena o array de retorno
-            if (a.chave == b.chave)
-                return 0;
-            if (a.chave > b.chave)
-                return 1;
-            return -1;
-        });
-        return retorno;
-    }
-
-
-
-    self.listadados = function () {
-        return self.db;
-    }
-
-
-    self.adicionar_rep = function (dado, chave) {//adiciona no banco matriz
-        exports.banco_matriz.db.push({ chave: chave, dado: dado });
-    }
-
-    self.alterar_rep = function (dado, chave) {//altera no banco matriz
-        for (var index in exports.banco_matriz.db) {
-            var registro = exports.banco_matriz.db[index];
-            if (registro.chave == chave) {
-                registro.dado = dado;
-            }
-        }
-    }
-    self.deletar_rep = function (chave) {//deleta no banco matriz
-        for (var index in exports.banco_matriz.db) {
-            var registro = exports.banco_matriz.db[index];
-            if (registro.chave == chave) {
-                delete exports.banco_matriz.db[index];
-            }
-        }
-    }
-
-    self.sync = function () {
-        for (i = self.indexLog; i < self.log.length; i++) {
-            var item = self.log[i];
-
-            if (item.operacao == 'adicionar') {
-                exports.banco_matriz.adicionar_rep(item.dado, item.chave);
-                exports.banco_matriz.log.push({ operacao: 'adicionar', chave: item.chave, dado: item.dado });//historico do banco matriz
-            }
-
-            else if (item.operacao == 'alterar') {
-                exports.banco_matriz.alterar_rep(item.dado, item.chave);
-                exports.banco_matriz.log.push({ operacao: 'alterar', chave: item.chave, dado: item.dado });//historico do banco matriz
-            }
-
-            else if (item.operacao == 'deletar') {
-                exports.banco_matriz.deletar_rep(item.chave);
-                exports.banco_matriz.log.push({ operacao: 'deletar', chave: item.chave, dado: item.dado });//historico do banco matriz
-            }
-        }
-        self.sync_matriz();//adiciona no banco as mensagens que foram enviado por outro banco
-        self.indexLog = self.db.length;//Seta o ultimo registro que foi enviado para o matriz
-    }
-
-
-    self.sync_matriz = function () {//Se tiver alguma mensagem para o banco que está consutando ele pega o dado e grava em seu banco
-        var outrosbancos = new Array();
-        for (var i in exports.banco_matriz.log) {
-            var item = exports.banco_matriz.log[i];
-            var subchave = item.chave;
-            if (self.nome_banco != subchave.slice(0, 1)) {
-                if (item.operacao == 'adicionar') {
-                    self.db.push({ chave: item.chave, dado: item.dado });
-                }
-
-                else if (item.operacao == 'alterar') {
-                    for (var index in self.db) {
-                        var registro = self.db[index];
-                        if (registro.chave == item.chave) {
-                            registro.dado = item.dado;
-                        }
-                    }
-                }
-
-                else if (item.operacao == 'deletar') {
-                    for (var index in self.db) {
-                        var registro = self.db[index];
-                        if (registro.chave == item.chave) {
-                            delete self.db[index];
-                        }
-                    }
-                }
-            }
-            else {
-                outrosbancos.push({ operacao: item.operacao, chave: item.chave, dado: item.dado });//Se não ele adiciona em outro array para os outros bancos, sem a mensagem que já foi sincronizada pelo banco atual
-            };
-        }
-        exports.banco_matriz.log = outrosbancos; //atribui ao log do banco matriz as mensagens que não foram sincronizadas.
-    }
 };
 
-
-
-exports.banco_matriz = new exports.QualiomDB("matriz");
+module.exports.QualiomDB = QualiomDB;
---
+++ static/Replicacao_Neto/Replicacao_Neto/server.js
@@ -0,0 +1,81 @@
+﻿#!/bin/env node
+
+var fs = require("fs");
+var host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var express = require("express");
+var mongodb = require('mongodb');
+
+var ClasseAplicacao=function()
+{
+    var self=this;
+  
+    self.ConfiguraExpress=function()
+    {
+  
+        self.appex = express();
+
+        self.appex.use(self.appex.router); //use both root and other routes below
+        self.appex.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+        self.appex.get("/", function(request, response){ //root dir
+            response.send("Hello-Sao Azul!!");
+        });
+
+        self.appex.get("/erro", function(request, response){ //root dir
+            response.send(erro);
+        });
+
+        self.appex.get("/gravar", function(request, response){ // grava item no servidor
+
+            var chave=request.param('chave');
+            var dado=request.param('dado');
+	
+            self.db_sao_azul.insert(
+              {"_id": chave, "dado": dado},
+              function(result)
+              {  
+                  response.send("gravado: "+chave+" "+dado+ " result="+result);
+              });	
+        });
+    };
+  
+    self.conecta_banco_dados=function(callback)
+    {
+  
+        // https://github.com/openshift-quickstart/openshift-mongo-node-express-example/blob/master/server.js  
+ 
+        var dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1",parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT || 27017));
+        self.db = new mongodb.Db('saoazul', dbServer, {safe:false, auto_reconnect: true});
+        var dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME || 'qualiom';
+        var dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD || 'qualiom';
+
+        //var dbServer = new mongodb.Server("127.0.0.1",27017);
+        //self.db = new mongodb.Db('saoazul', dbServer, {safe:false, auto_reconnect: true});
+        //var dbUser = 'qualiom';
+        //var dbPass = 'qualiom';
+
+        self.db.open(function(err, db_open){
+            if(err){
+                erro=err;
+                callback();
+            };
+            self.db.authenticate(dbUser, dbPass, {authdb: "admin"}, function(err, res){
+                if(err){ erro=err; };
+                self.db_sao_azul = self.db.collection('saoazul');
+                callback();		
+            });
+        });
+    };
+  
+    self.inicia_servidor=function()
+    {
+        self.ConfiguraExpress();
+        self.appex.listen(port, host);
+    }
+};
+
+var app=new ClasseAplicacao();
+app.conecta_banco_dados(app.inicia_servidor);
+
+
