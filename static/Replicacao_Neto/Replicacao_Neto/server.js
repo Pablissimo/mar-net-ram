3d13cb442c68c3804172cd58f87ebf7b4fa39dc2
Charles Danillo <danillocharles@gmail.com>
2014-04-29 09:46:26 -0300

    Mongoose

--- static/Replicacao_Neto/Replicacao_Neto/server.js
+++ static/Replicacao_Neto/Replicacao_Neto/server.js
@@ -1,4 +1,6 @@
-﻿var fs = require("fs");
+﻿#!/bin/env node
+
+var fs = require("fs");
 var host = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var express = require("express");
