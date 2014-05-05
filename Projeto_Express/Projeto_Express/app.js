02398d6f329ceb090bd30e949530e744190bff47
joseaugustoneto <joseneto.design@gmail.com>
2014-05-04 22:01:14 -0300

    Sync

--- Projeto_Express/Projeto_Express/app.js
+++ Projeto_Express/Projeto_Express/app.js
@@ -5,6 +5,6 @@ app.get('/hello.txt', function (req, res) {
     res.send('Teste de Requisição HTTP');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(2000, function () {
     console.log('Escutando a porta %d', server.address().port);
 });
