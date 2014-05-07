f137eb5b1d17076050815b482218d8c04d96b8f6
joseaugustoneto <joseneto.design@gmail.com>
2014-05-07 00:17:57 -0300

    TypeScript Acessando radio Buttoms

---
+++ static/Modularizacao/TypeScriptHTMLApp1/identificador.ts
@@ -0,0 +1,9 @@
+﻿import Classe_Soma = Module_soma.Classe_Soma;
+
+var a, b;
+if ($('#navegador').prop('checked') == true)
+{
+    var classe_soma = new Classe_Soma;
+    classe_soma.somar(a, b);
+}
+
