e5600112bfc8eb8c8fdd97c13ca9eb7ffb8a5810
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-05-08 11:45:18 -0300

    Criação de Pacote qualiom_require

---
+++ static/Modularizacao/RequireDefineExport/identificador.ts
@@ -0,0 +1,22 @@
+﻿import mod_soma = require('modulo_soma');
+
+var obj = new mod_soma.Module_soma.Classe_Soma();
+
+$("#calcular").click(calcular);
+
+function calcular() {
+
+    if ($('#navegador').prop('checked') == true) {
+        var a:number = $("#A").val();
+        var b:number = $("#B").val();
+        var objeto:any = { a: a, b: b };
+        var objeto_json:string = JSON.stringify(objeto);
+        var resultado: any = obj.somar(objeto_json);
+
+        var resultado_objeto: any = JSON.parse(resultado);
+
+        $("#R").val(resultado_objeto.resultado.toString());
+    }
+    else if ($('#servidor').prop('checked') == true) {
+    }
+};
---
+++ static/Modularizacao/RequireDefineExport/modulo_soma.ts
@@ -0,0 +1,17 @@
+﻿module Module_soma {
+
+    export class Classe_Soma {
+
+        public somar(json: string) {
+            var objeto: any = JSON.parse(json);
+            var a: number = objeto.a;
+            var b: number = objeto.b;
+            var resultado: number = a + b;
+
+            var retorno_objeto: any = { resultado: resultado };
+            var retorno_json: string = JSON.stringify(retorno_objeto);
+            return retorno_json;
+        }
+
+    }
+}
