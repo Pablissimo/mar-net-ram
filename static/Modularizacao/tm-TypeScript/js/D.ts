bbdfeeedf42be4a7e8ced980be55f24e88db3bd0
Charles Danillo <danillocharles@gmail.com>
2014-05-08 12:02:12 -0300

    171

--- static/Modularizacao/tm-TypeScript/js/D.ts
+++ static/Modularizacao/tm-TypeScript/js/D.ts
@@ -1,9 +1,10 @@
 import mod_E = require('E');
+export module A {
+    if (mod_E.OK != "OK")
+        alert("ERRO NO E");
 
-if (mod_E.OK != "OK")
-    alert("ERRO NO E");
+    export var D = "OK";
+    export var OK = "OK";
 
-export var D = "OK";
-export var OK = "OK";
-
-document.write('D');
+    document.write('D');
+}
