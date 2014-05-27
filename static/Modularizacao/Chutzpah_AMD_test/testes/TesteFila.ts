a7929d25ef9f24b29f80793f95a59f7c6b23e40b
qualiom <danillocharles@gmail.com>
2014-05-27 12:35:04 -0300

    sync

--- static/Modularizacao/Chutzpah_AMD_test/testes/TesteFila.ts
+++ static/Modularizacao/Chutzpah_AMD_test/testes/TesteFila.ts
@@ -2,7 +2,7 @@
 
 import Fila = require("../../Chutzpah_AMD_app/services/Pilha");
 
-describe("Pilha", () => {
+describe("Fila", () => {
     var fila: Fila;
     beforeEach(() => {
         //ARRANGE
@@ -13,5 +13,14 @@ describe("Pilha", () => {
     it("Quando a Fila for criada", () => {
         //ACT      
             expect(fila.itens_na_fila()).toBe(undefined);
+    });
+
+    it("Quando a Fila estiver vazia", () => {
+        //ACT      
+        expect(fila.itens_na_fila()).toBe(undefined);
+    });
+    it("Quando a vazia", () => {
+        //ACT      
+        expect(fila.itens_na_fila()).toBe(undefined);
     });
 });
