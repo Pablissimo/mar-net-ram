b29f2113689aeb62f915cae5131686b812cb1c78
qualiom <danillocharles@gmail.com>
2014-05-27 12:17:45 -0300

    Pilha e Fila Testes no Test Explore

--- static/Modularizacao/Chutzpah_AMD_app/services/Pilha.ts
+++ static/Modularizacao/Chutzpah_AMD_app/services/Pilha.ts
@@ -8,6 +8,11 @@
         return itens_pilha;
     }
 
+    itens_na_fila() {
+        var itens_fila = this.items_fila;
+        return itens_fila;
+    }
+
     empilhar(item: any) {
         this.items_pilha.push(item);
     }
---
+++ static/Modularizacao/Chutzpah_AMD_test/testes/TesteFila.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../lib/jasmine/jasmine.d.ts" />
+
+import Fila = require("../../Chutzpah_AMD_app/services/Pilha");
+
+describe("Pilha", () => {
+    var fila: Fila;
+    beforeEach(() => {
+        //ARRANGE
+
+        fila = new Fila();
+    });
+
+    it("Quando a Fila for criada", () => {
+        //ACT      
+            expect(fila.itens_na_fila()).toBe(undefined);
+    });
+});
