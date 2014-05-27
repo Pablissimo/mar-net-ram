93740491778a04575cc13895bffe01333cbef54a
qualiom <danillocharles@gmail.com>
2014-05-27 12:08:05 -0300

    Chutzpah Testes Funcionando

---
+++ static/Modularizacao/Chutzpah_AMD_test/obj/Debug/config.js
@@ -0,0 +1,7 @@
+requirejs.config({
+    paths: {
+        //because of TestHarnessLocationMode - SettingsFileAdjacent all references here are relative to
+        //chutzpah.json file location
+        'knockout': 'Chutpah_AMD_app/lib/knockout'
+    }
+});
