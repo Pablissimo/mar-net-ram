11a107c39c848125843004719f751d9b0a42e140
qualiom <danillocharles@gmail.com>
2014-05-28 12:16:14 -0300

    Famous

---
+++ WebComponentes/Famous/app.js
@@ -0,0 +1,16 @@
+﻿var Engine = require("famous/core/Engine");
+var Surface = require("famous/core/Surface");
+
+var mainContext = Engine.createContext();
+
+var surface = new Surface({
+    size: [200, 200],
+    content: "Hello World",
+    classes: ["red-bg"],
+    properties: {
+        lineHeight: "200px",
+        textAlign: "center"
+    }
+});
+
+mainContext.add(surface);
