3e15a80d3d6621cd60976907637c6e8d0599b859
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-05-30 12:49:07 -0300

    commit2

---
+++ WebComponentes/Famous_Neto_Ramon/src/app/header.js
@@ -0,0 +1,46 @@
+﻿define(function (require, exports, module) {
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var View = require('famous/core/View');
+    var Transform = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
+
+    var header = Engine.createContext();
+
+    function Header() {
+        View.apply(this, arguments);
+
+        _create_header.call(this);
+
+    }
+
+    Header.prototype = Object.create(View.prototype);
+    Header.prototype.constructor = Header;
+
+    function _create_header() {
+        var fundo = new Surface({
+            size: [undefined, 44],
+            classes: ['header_top'],
+            properties: {
+                backgroundColor: 'red',
+                zIndex: -3,
+            }
+        });
+        header.add(fundo).add(_criar_nav.call(this));
+    }
+
+    var Nav = require('./nav');
+
+    function _criar_nav() {
+        this.nav = new Nav();
+
+        var navModifier = new StateModifier({
+            transform: Transform.behind
+        });
+
+        header.add(navModifier).add(this.nav);
+    }
+
+   
+    module.exports = Header;
+});
---
+++ WebComponentes/Famous_Neto_Ramon/src/main.js
@@ -0,0 +1,10 @@
+﻿define(function (require, exports, module) {
+    var Engine = require('famous/core/Engine');
+    var Appview = require('./app/appview');
+
+    var mainContext = Engine.createContext();
+    var appview = new Appview();
+    mainContext.add(appview);
+
+
+});
