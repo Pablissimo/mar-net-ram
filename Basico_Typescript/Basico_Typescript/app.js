a76a6846dbc81fde4797383bcf4e74eebdd0e8ad
José Augusto Batista Neto <joseneto.design@gmail.com>
2014-06-04 07:11:00 -0300

    revert
    motivo: Ao criar um projeto inicial no WebStorm ele moveu os arquivos e não copiou,ficar atento para esta questão

---
+++ Basico_Typescript/Basico_Typescript/app.js
@@ -0,0 +1,39 @@
+﻿console.log('Hello world');
+
+//criação de Variavel tipo Boolean
+var tipo_Boolean = true;
+
+//criação de Variavel tipo numerido
+var tipo_numerico = 6;
+
+//criação de Variavel tipo String
+var tipo_string = 'neto';
+
+//criação de Variavel tipo Array
+var tipo_array = [1, 0, 10];
+tipo_array.add1();
+
+//criação de Variavel tipo Enum
+var Cores;
+(function (Cores) {
+    Cores[Cores["Vermelho"] = 0] = "Vermelho";
+    Cores[Cores["Amarelo"] = 1] = "Amarelo";
+    Cores[Cores["Azul"] = 2] = "Azul";
+})(Cores || (Cores = {}));
+;
+var c = 1 /* Amarelo */;
+
+//Criação de Classes
+var Matriz = (function () {
+    function Matriz(nome) {
+        this.contador = 0;
+        this.db = [];
+        this.nome_banco = nome;
+    }
+    return Matriz;
+})();
+
+function adicionar(dado) {
+    var recebe_dado = dado;
+}
+//# sourceMappingURL=app.js.map
