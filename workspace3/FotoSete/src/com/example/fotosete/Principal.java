9bf6597dc97044eb01b5840d210fe5b8df63e09e
MarcelloVSilva <mvnota10@gmail.com>
2014-06-27 19:38:23 -0300

    teste

--- workspace3/FotoSete/src/com/example/fotosete/Principal.java
+++ workspace3/FotoSete/src/com/example/fotosete/Principal.java
@@ -25,7 +25,8 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
-@SuppressLint("NewApi") public class Principal extends Activity implements SurfaceHolder.Callback {
+@SuppressLint("NewApi") 
+public class Principal extends Activity implements SurfaceHolder.Callback {
 
 
 	
@@ -111,13 +112,14 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder,  int format, int width, int height) {
-		System.out.println("ZERO");
 		if (holder.getSurface() != null) {
 			try {
 				filmadoraCamera.stopPreview();
+			
 			} catch (Exception e) {
 			}
 			try {
+
 				filmadoraCamera.setPreviewDisplay(holder);
 				filmadoraCamera.startPreview();
 			} catch (IOException e) {
@@ -136,15 +138,15 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 			filmadoraCamera.stopPreview();
 		}
 	}
-	@TargetApi(Build.VERSION_CODES.GINGERBREAD) @SuppressLint("NewApi") public static void setCameraDisplayOrientation(Activity activity,
-	         int cameraId, android.hardware.Camera camera) {
+	@TargetApi(Build.VERSION_CODES.GINGERBREAD) @SuppressLint("NewApi") 
+	public static void setCameraDisplayOrientation(Activity activity,int cameraId, android.hardware.Camera camera) {
 	     android.hardware.Camera.CameraInfo info =new android.hardware.Camera.CameraInfo();
 	     android.hardware.Camera.getCameraInfo(cameraId, info);
 	     int rotation = activity.getWindowManager().getDefaultDisplay()
 	             .getRotation();
 	     int degrees = 0;
 	     switch (rotation) {
-	         case Surface.ROTATION_0: degrees = 0; break;
+	         case Surface.ROTATION_0: degrees = 90; break;
 	         case Surface.ROTATION_90: degrees = 90; break;
 	         case Surface.ROTATION_180: degrees = 180; break;
 	         case Surface.ROTATION_270: degrees = 270; break;
