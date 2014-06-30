251b51fb1ca559a82da1fdc9b7db74ea0f4bc7a4
qualiom <danillocharles@gmail.com>
2014-06-30 09:07:55 -0300

    teste

--- static/Modularizacao/TypeScriptHTMLApp1/modulo_soma.js
+++ static/Modularizacao/TypeScriptHTMLApp1/modulo_soma.js
@@ -1,15 +1,13 @@
-﻿define(["require", "exports"], function(require, exports) {
-    (function (Module_soma) {
-        var Classe_Soma = (function () {
-            function Classe_Soma() {
-            }
-            Classe_Soma.prototype.somar = function (a, b) {
-                return a + b;
-            };
-            return Classe_Soma;
-        })();
-        Module_soma.Classe_Soma = Classe_Soma;
-    })(exports.Module_soma || (exports.Module_soma = {}));
-    var Module_soma = exports.Module_soma;
+﻿ // adaptação qualiom 
+define("../TypeScriptHTMLApp1/modulo_soma",["require", "exports"], function(require, exports) {
+    var Classe_Soma = (function () {
+        function Classe_Soma() {
+        }
+        Classe_Soma.prototype.somar = function (a, b) {
+            return a + b;
+        };
+        return Classe_Soma;
+    })();
+    exports.Classe_Soma = Classe_Soma;
 });
 //# sourceMappingURL=modulo_soma.js.map
